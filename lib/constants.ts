405b9eb3351b038f4c0f143caf36070c8b94ef16
Hussein Rashid <husseinrashiduni@gmail.com>
2026-07-23 19:10:59 +0300

    Add ticket page size constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,3 +16,7 @@ export const TICKET_CATEGORIES = [
 export type TicketStatus = (typeof TICKET_STATUSES)[number];
 export type TicketPriority = (typeof TICKET_PRIORITIES)[number];
 export type TicketCategory = (typeof TICKET_CATEGORIES)[number];
+
+export const TICKET_PAGE_SIZES = [5, 10, 25, 50, 100] as const;
+export type TicketPageSize = (typeof TICKET_PAGE_SIZES)[number];
+export const DEFAULT_TICKET_PAGE_SIZE: TicketPageSize = 25;
