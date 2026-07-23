2b214357f76054f3a02f669920d8f3f895808a19
Hussein Rashid <husseinrashiduni@gmail.com>
2026-07-23 19:40:42 +0300

    Default ticket list to 5 rows per page

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,4 +19,4 @@ export type TicketCategory = (typeof TICKET_CATEGORIES)[number];
 
 export const TICKET_PAGE_SIZES = [5, 10, 25, 50, 100] as const;
 export type TicketPageSize = (typeof TICKET_PAGE_SIZES)[number];
-export const DEFAULT_TICKET_PAGE_SIZE: TicketPageSize = 25;
+export const DEFAULT_TICKET_PAGE_SIZE: TicketPageSize = 5;
